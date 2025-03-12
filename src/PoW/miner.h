ce78edbf6ebe5250e4d5ac855e3f5607e9f65a56
Qmorris66 <mickhail6v@gmail.com>
2025-03-12 23:21:08 +0400

    Start work with block: more about Lumma block

--- src/PoW/miner.h
+++ src/PoW/miner.h
@@ -2,10 +2,14 @@
 // Created by iqment on 3/11/25.
 //
 
+#pragma once
+
 #ifndef LUMMAENGINE_MINER_H
 #define LUMMAENGINE_MINER_H
 
+
 #include "../node/node.h"
+#include "../block/Block.hpp"
 #include <sodium.h>
 #include <sodium/crypto_sign_ed25519.h>
 #include <sodium/crypto_core_ed25519.h>
@@ -13,9 +17,17 @@
 #include <vector>
 #include <string>
 
+#define SYMBOL 0
+#define BGN 0ULL
+
+typedef unsigned long long nonce;
+
 class Miner: protected Node{
 private:
-    std::vector<uint8_t>
+    std::vector<uint8_t> empty;
+
+public:
+    nonce work_2_block(Block::Block *block);
 };
 
 #endif //LUMMAENGINE_MINER_H
