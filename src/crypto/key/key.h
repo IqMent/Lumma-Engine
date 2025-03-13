ae26c41e0527a68ed407a2b77a1a591d8cbfba4a
Qmorris66 <mickhail6v@gmail.com>
2025-03-14 02:18:21 +0400

    F*ck

--- src/crypto/key/key.h
+++ src/crypto/key/key.h
@@ -5,9 +5,47 @@
 #ifndef LUMMAENGINE_KEY_H
 #define LUMMAENGINE_KEY_H
 
+#include <unordered_map>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <fstream>
+#include <cstdint>
+
+#ifdef _WIN32
+    #define PATH "pass"
+#elif defined(__linux__)
+    #define PATH "~/.LummaEngine/keys/"
+#elif defined(__APPLE__)
+    #define PATH "~/pass"
+#endif
 
 namespace Crypto{
-    class KeyPair{
+    typedef struct{
+        std::vector<uint8_t> pubKey;
+        std::vector<uint8_t> privKey;
+        std::vector<uint8_t> shortPubKey;
+    } secp256k1_key_pair;
+}
+
+namespace Crypto{
+    class secp256k1_KeyPair{
+    public:
+        std::unordered_map<std::string, std::vector<uint8_t>> generate_keypair(std::vector<uint8_t> seed);
+        std::unordered_map<std::string, std::vector<uint8_t>> generate_keypair();
+    public:
+        secp256k1_key_pair *generate_keys(std::vector<uint8_t> seed);
+        secp256k1_key_pair *generate_keys();
+    };
+}
+
+namespace Crypto{
+    class ed25519_KeyPair{
+    public:
+        std::unordered_map<std::string, std::vector<uint8_t>> generate_keypair(std::vector<u_int8_t> seed);
+        std::unordered_map<std::string, std::vector<uint8_t>> generate_keypair();
+        bool save_keypair(std::unordered_map<std::string, std::vector<uint8_t>> *kp);
+    private:
 
     };
 }
