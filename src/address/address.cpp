398a91b45566c4c6c44a264cbd135c801f42291a
Qmorris66 <mickhail6v@gmail.com>
2025-03-17 06:04:47 +0400

    Bug fix

--- src/address/address.cpp
+++ src/address/address.cpp
@@ -7,7 +7,6 @@
 
 std::vector<u_int8_t> Address::Stage0::get_address_from_bn(unsigned char *pk) {
     std::vector<uint8_t> address;
-    address.push_back(PREFIX);
     Crypto::evp_SHA256 sha256;
     unsigned char *sha256_address = sha256.sha256(pk);
     Crypto::evp_KECCAK256 keccak256;
@@ -15,7 +14,6 @@ std::vector<u_int8_t> Address::Stage0::get_address_from_bn(unsigned char *pk) {
     for (int i = 0; i < 32; i++) {
         address.push_back(keccak256_address[i]);
     }
-//    address.push_back(ENDFIX);
     delete sha256_address;
     delete keccak256_address;
     return address;
